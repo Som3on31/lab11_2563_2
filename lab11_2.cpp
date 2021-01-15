3edecd049ce07b5c69fb739fe8608e49c2577162
Som3on31 <Som3on31_kek@users.noreply.github.com>
2021-01-15 16:24:23 +0700

    Completed gcd.

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 using namespace std;
 
+long long int gcd(int &,int &);
+
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+long long int gcd(int &a,int &b){
+    if (a%b==0) return b;
+    else{
+        a=a%b;
+        return gcd(b,a);
+    }
 }
