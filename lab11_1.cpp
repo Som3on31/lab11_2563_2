d97ef9a38b9c8083e5199dc084d63a8da8138fc8
Som3on31 <Som3on31_kek@users.noreply.github.com>
2021-01-15 15:30:43 +0700

    Completed fibonacci function.

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,9 +1,21 @@
 #include<iostream>
 using namespace std;
 
+long long int fibonacci(int);
+
+
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+long long int fibonacci(int a){
+
+    if(a==0) return 0;
+    else if (a==1) return 1;
+    else return fibonacci(a-1)+fibonacci(a-2);
+
 }
